b1d3efdfd1cdda2d555416fe93d59ecf03449892
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-02-25 15:42:52 +0700

    Step 5 - Semantics trees

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,9 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -43,7 +46,13 @@ class TopAppBarTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
